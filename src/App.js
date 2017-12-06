7e3d5b25c4969cd559942bcb6d52c46d26bc5868
Sumit Kumar Tiwari <SumitTiwari203@gmail.com>
2017-12-06 18:39:07 -0500

    Added AJAX request code

--- src/App.js
+++ src/App.js
@@ -1,36 +1,26 @@
 import React, { Component } from 'react'
 import ListContacts from './ListContacts'
+import * as ContactAPI from './utils/ContactsAPI'
 
 
 class App extends Component {
   state ={
-      contacts : [
-          {
-              "id": "ryan",
-              "name": "Ryan Florence",
-              "email": "ryan@reacttraining.com",
-              "avatarURL": "http://localhost:5001/ryan.jpg"
-          },
-          {
-              "id": "michael",
-              "name": "Michael Jackson",
-              "email": "michael@reacttraining.com",
-              "avatarURL": "http://localhost:5001/michael.jpg"
-          },
-          {
-              "id": "tyler",
-              "name": "Tyler McGinnis",
-              "email": "tyler@reacttraining.com",
-              "avatarURL": "http://localhost:5001/tyler.jpg"
-          }
-      ]
+      contacts : []
   }
 
   removeContact =(contact)=>{
-    this.setState((state)=>({
-        contacts:state.contacts.filter((c)=>c.id!==contact.id)
-    }))
-  }
+       ContactAPI.remove(contact).then((contact)=>{
+           this.setState((state)=>({
+               contacts:state.contacts.filter((c)=>c.id!==contact.id)
+           }))
+       })
+    }
+
+  componentDidMount(){
+        ContactAPI.getAll().then((contacts)=>{
+            this.setState({contacts})
+        })
+    }
 
   render() {
     return (
