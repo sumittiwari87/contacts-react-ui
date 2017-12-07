900e7ba35458f6a8a27e9f9a5a68bec58576f625
Sumit Kumar Tiwari <SumitTiwari203@gmail.com>
2017-12-06 21:45:20 -0500

    Added Display count and clear

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -39,6 +39,10 @@ class ListContacts extends Component{
         query:''
     }
 
+    clearQuery = ()=>{
+        this.setState({query:''})
+    }
+
     updateQuery = (query)=>{
         this.setState({query:query.trim()})
     }
@@ -60,12 +64,19 @@ class ListContacts extends Component{
             <div className="list-contacts">
                 <div className="list-contacts-top">
                     <input className="search-contacts"
-                            placeholder="search contact"
-                            type="text"
-                            value={this.state.query}
-                            onChange={(event)=> this.updateQuery(event.target.value)}
+                           placeholder="search contact"
+                           type="text"
+                           value={this.state.query}
+                           onChange={(event)=> this.updateQuery(event.target.value)}
                     />
                 </div>
+                {showingContacts.length!== contacts.length && (
+                    <div className="showing-contacts">
+                        <span>Now showing {showingContacts.length} of {contacts.length}</span>
+                        <button onClick={this.clearQuery}>Show all</button>
+                    </div>
+                )}
+
                 <ol className="contact-list">
                     {showingContacts.map((contact) => (
                         <li key={contact.id} className="contact-list-item">
